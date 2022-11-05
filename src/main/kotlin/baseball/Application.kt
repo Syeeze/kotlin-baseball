adfb826dffc3d4aacf4abd8dbb5e224e0879b317
Seyong <38519706+Syeeze@users.noreply.github.com>
2022-11-06 01:44:13 +0900

    랜덤 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
 }
+
+fun randomNum() : Int {
+    var num = Randoms.pickNumberInRange(100,999)
+
+    return num
+}
+
