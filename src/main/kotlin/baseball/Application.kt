54ce56e8bddfd8ca19969e71ae1d80e325708ef3
Seyong <38519706+Syeeze@users.noreply.github.com>
2022-11-06 01:53:33 +0900

    숫자 입력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,14 +1,20 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     //TODO("프로그램 구현")
+    print(inputNum())
 }
 
 fun randomNum() : Int {
     var num = Randoms.pickNumberInRange(100,999)
-
     return num
+}
+
+fun inputNum(): Int {
+    var inputNum = Console.readLine().toInt()
+    return inputNum
 }
 
