c8321f6f787db2998478e3ee54c0605b06e4a642
Seyong <38519706+Syeeze@users.noreply.github.com>
2022-11-08 02:42:49 +0900

    스트라이 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,16 +5,50 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     //TODO("프로그램 구현")
-    print(inputNum())
+    val randomNum = randomNum()
+    println("숫자 야구 게임을 시작합니다.")
+    println(randomNum)
+    comparing(randomNum, inputNum())
+
 }
 
-fun randomNum() : Int {
-    var num = Randoms.pickNumberInRange(100,999)
+fun randomNum() : String {
+    var num = Randoms.pickNumberInRange(100,999).toString()
     return num
 }
 
-fun inputNum(): Int {
-    var inputNum = Console.readLine().toInt()
+fun inputNum(): CharArray {
+    print("숫자를 입력해주세요 : ")
+    var inputNum = Console.readLine().toCharArray()
     return inputNum
+}
+
+fun comparing(randomNum: String, inputNum: CharArray) {
+    val randomNum = randomNum.toCharArray()
+    val inputNum =  inputNum
+    var count = '0'
+
+    strike(randomNum,inputNum)
+
+//    for(i in 0..2) {
+//        if(inputNum[i] == randomNum[0])
+//            count++
+//        if(inputNum[i] == randomNum[1])
+//            count++
+//        if(inputNum[i] == randomNum[2])
+//            count++
+//    }
+//
+   //print(count)
+}
+
+fun strike(randomNum: CharArray, inputNum: CharArray): Int {
+    var count = 0
+
+    for(i in 0..2)
+        if (randomNum[i] == inputNum[i])
+            count++
+    print(count)
+    return count
 }
 
